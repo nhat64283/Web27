799e5507e66b2acccd50f305e54b8856c34dedab
nhat64283 <52846279+nhat64283@users.noreply.github.com>
2019-07-24 00:19:04 +0700

    Add files via upload

--- intro.js
+++ intro.js
@@ -10,6 +10,13 @@ registerButton.addEventListener('click',() => {
         const errorMessageElement1 = document.querySelector('.errorMessage1');
         errorMessageElement1.innerText = '!Please input';
     }
+    const useremail= document.querySelector('.register-input4');
+    const usere = useremail.value;
+    if(!usere)
+    {
+        const errorMessageElement4 = document.querySelector('.errorMessage4');
+        errorMessageElement4.innerText = '!Please input';
+    }
     const userpassword= document.querySelector('.register-input2');
     const userp = userpassword.value;
     if(!userp)
@@ -17,11 +24,19 @@ registerButton.addEventListener('click',() => {
         const errorMessageElement2 = document.querySelector('.errorMessage2');
         errorMessageElement2.innerText = '!Please input';
     }
-    const userrepeatpassword = document.querySelector('.errorMessage3');
+
+    const userrepeatpassword = document.querySelector('.register-input3');
     const userrp = userrepeatpassword.value;
-    if(!userrp)
+    if((!userrp) || (userrp!=userp))
     {
         const errorMessageElement3 = document.querySelector('.errorMessage3');
         errorMessageElement3.innerText = '!Please input';
+    }
+    if((!username) || (!usere) || (!userp) || (!userrp) || (userrp!=userp)){
+        const mes=document.querySelector('.message');
+        mes.innerText='Please fullfill what is required!';
+    }
+    else{
+        mes.innerText='You have fullfilled what is required.';
     }
 });
